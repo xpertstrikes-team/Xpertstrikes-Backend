bcba0ac9e330fa285b953716da68c250eb008119
Sanjay <0sanjay0@gmail.com>
2025-11-24 14:21:08 +0530

    admin page added

--- routes/adminAuthRoutes.js
+++ routes/adminAuthRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bcrypt from "bcryptjs";
-import Admin from "../models/Admin.js";
+import Admin from "../models/admin.js";
 
 const router = express.Router();
 
